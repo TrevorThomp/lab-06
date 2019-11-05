c74a3f8a44ff8e5a3db33688067759b1d4e28183
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-11-05 09:19:40 -0800

    completed map feature to display results from server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const express =require('express');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
@@ -15,16 +15,11 @@ app.use(cors());
 app.use(express.static('./front-end'));
 
 app.get('/location', (request,response) => {
-  try {
-    const geoData = require('./data/geo.json');
-    const city = request.query.data;
-    const locationData = new Location(city,geoData);
-    response.send(locationData);
-  }
-  catch(error) {
-    errorHandler('Something went wrong', request,response);
-  }
-})
+  const geoData = require('./data/geo.json');
+  const city = request.query.data;
+  const locationData = new Location(city,geoData);
+  response.send(locationData);
+});
 
 function Location(city, geoData) {
   this.search_query = city;
@@ -41,4 +36,7 @@ function errorHandler(error,request,response) {
   response.status(500).send(error);
 }
 
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
+
+
